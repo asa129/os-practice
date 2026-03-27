b12295d9bc7a3af22e9a64660ac288d78756bae9
asa129 <asa.jad1290@gmail.com>
2026-03-27 23:03:46 +0900

    フレームバッファを全て白色にする

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@
 #[no_mangle]
 fn efi_main() {
     // println!("Hello, world!");
+    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
+    let vram_abbr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
+    let vram = unsafe {
+        slice::from_raw_parts_mut(vram_abbr as *mnt u32, vram_byte_size / size_of::<u32>())
+    };
+    for e in vram {
+        *e = 0xffffff;
+    }
     loop {}
 }
 
